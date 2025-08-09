b0285e0d357b07bc94eb9036af6e9ecd7aa2360c
Eric Mbithi <mbithieri@gmail.com>
2025-08-09 23:59:20 +0300

    prompt player for rock, paper, or scissors

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ def get_choices():
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = { "player": player_choice, "computer": computer_choice }
+  return choices
+
+def check_win(player, computer):
+  print(f"you chose {player}, computer chose {computer}")
+  if player == computer:
+    return "Its a tie!"
+  elif player == "rock":
+    if computer == "scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers rock! You lose."
+  elif player == "paper":
+    if computer == "rock":
+      return "paper covers rock! You win!"
+    else:
+      return "scissors cuts paper! You lose."
+  elif player == "scissors":
+    if computer == "paper":
