6586e11e7c0dbe0ee2c025baaff6458dff616077
Eric Mbithi <mbithieri@gmail.com>
2025-08-09 23:57:44 +0300

    Define get_choices() function

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import random
+
+def get_choices():
+  player_choice = input("Enter a choice ( rock, paper, scissors): ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = { "player": player_choice, "computer": computer_choice }
